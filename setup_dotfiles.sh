1f3835f0a8bf8b2a9583f44d8532f03e22d3d10e
takeoshimizu <st0098@gmail.com>
2015-02-18 18:14:14 +0900

    NeoBundleのインストールも一緒にするように変更

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,3 +4,7 @@ for dotfile in ${DOT_FILES[@]}
 do
   ln -s ~/dotfiles/$dotfile ~/$dotfile
 done
+
+# Install NeoBundle
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
